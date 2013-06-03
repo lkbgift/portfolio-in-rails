150dff165deb11860e30fb0588a079e87eca997d
lkbgift <lkbgift@gmail.com>
2013-06-03 16:41:25 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portrails::Application.config.secret_token = '2642d1948858b44062da3093a5ed837220ec00ccfe4cdd18a9c1e4d14d14c96830581642ad1bf13580cb4197dcb17390418f29a0069616b3417599398c450aab'
