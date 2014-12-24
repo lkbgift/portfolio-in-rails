aca2b7aaefdb2d52d9e371ec70f209c956980bec
rememberlenny <rememberlenny@gmail.com>
2014-12-24 13:14:31 -0500

    Create a callback

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,14 @@
 class Portfolio < ActiveRecord::Base
+  validates :slug, presence: true
+  before_validation :ensure_slug_exists
+
+  protected
+    def ensure_slug_exists
+      if slug.nil?
+        title = self.name
+        split_title = title.split(' ')
+        self.slug = split_title.join('-')
+      end
+    end
   attr_accessible :date, :description, :image, :role, :title, :gifurl, :hasgif
 end
