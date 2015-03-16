ff46106eb26faf645edcc6013c3ec4be300a6905
rememberlenny <rememberlenny@gmail.com>
2015-03-16 09:26:21 -0400

    Add the date to the portfolio idem

--- app/models/creative.rb
+++ app/models/creative.rb
@@ -1,3 +1,3 @@
 class Creative < ActiveRecord::Base
-  attr_accessible :description, :image, :title, :url, :date_start, :date_end
+  attr_accessible :description, :image, :title, :url, :date_started, :date_ended
 end
