a40397854aaaef6d747d6f63aa625592b278aa0d
rememberlenny <leonard_bogdonoff@condenast.com>
2015-05-05 11:21:59 -0400

    Change asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Portrails::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  config.action_controller.asset_host = 'blog.rememberlenny.com'
+
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
